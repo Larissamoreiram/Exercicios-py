4c48b4c22635f98455048f364ed40e4d0d37f3bc
Larissa Moreira <64422214+Larissamoreiram@users.noreply.github.com>
2020-09-28 12:11:32 -0300

    Create exercicio02.py

---
+++ exercicio02.py
@@ -0,0 +1,20 @@
+'''
+TABELA DE PRODUTOS
+'''
+produtos = ['Tablet', 'Teclado', 'Impressora', 'Monitor']
+print('Relação de produtos: ')
+for indice in produtos:
+    print(indice)
+    print ('-'*20)
+    print ()
+
+    '''
+
+produtos
+
+0 - mouse
+1 - teclado
+2 - impressora
+3 - monitor 
+
+    '''
