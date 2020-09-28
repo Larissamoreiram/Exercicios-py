5bc6e25dcf8ed8d4a125b584de996873865963c9
Larissa Moreira <64422214+Larissamoreiram@users.noreply.github.com>
2020-09-28 12:12:47 -0300

    Create exercicio03.py

---
+++ exercicio03.py
@@ -0,0 +1,19 @@
+'''
+PRODUTO NÃO ENCONTRADO
+'''
+produtos = ['tablet', 'teclado', 'impressora', 'monitor']
+if 'mouse' in produtos:
+    print('Sim, ‘monitor’ está na lista de produtos')
+else:
+    print('Produto não encontrado')
+
+'''
+
+produtos
+
+0 - mouse
+1 - teclado
+2 - impressora
+3 - monitor 
+
+'''
