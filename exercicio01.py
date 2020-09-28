cb3d2a6f5908f09eaa7499e913e233aac428f971
Larissa Moreira <64422214+Larissamoreiram@users.noreply.github.com>
2020-09-28 12:09:54 -0300

    Create exercicio01.py

---
+++ exercicio01.py
@@ -0,0 +1,17 @@
+'''
+POSIÇÃO DOS PRODUTOS
+'''
+produtos = ['tablet', 'teclado', 'impressora', 'monitor']
+for indice in range(4):
+    print('Produto.....:', produtos[indice], 'na posição: ', indice)
+
+  '''
+  
+produtos
+
+0 - mouse
+1 - teclado
+2 - impressora
+3 - monitor
+   
+   '''
